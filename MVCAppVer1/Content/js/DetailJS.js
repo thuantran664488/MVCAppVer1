9f5d982f361f5bba581b67ddb4ffcde6c017efc2
thuantran664488 <trandinhthuan0112@gmail.com>
2021-07-13 10:49:51 +0700

    add responsive for index

--- MVCAppVer1/Content/js/DetailJS.js
+++ MVCAppVer1/Content/js/DetailJS.js
@@ -1,12 +1,17 @@
 ﻿let inputValue = document.getElementById('inputValue');
 var newVal;
+
+function handleChange() {
+    inputValue = document.getElementById('inputValue');
+}
+
 function decreaseValue() {
     if (inputValue.value > 1) {
         newVal = Number(inputValue.value) - 1;
     } else newVal = 1;
     inputValue.setAttribute('value', newVal);
-}
+    }
 function increaseValue() {
-    var newVal = Number(inputValue.value) + 1;
+    newVal = Number(inputValue.value) + 1;
     inputValue.setAttribute('value', newVal);
 }
--- MVCAppVer1/Content/js/IndexJS.js
+++ MVCAppVer1/Content/js/IndexJS.js
@@ -1,7 +1,7 @@
 ﻿let productName = document.getElementsByClassName("product-name");
 for (let i = 0; i < productName.length; i++) {
     proElem = productName[i].innerHTML;
-    if (proElem.length >= 45) {
-        productName[i].innerHTML = proElem.slice(0, 45) + " ...";
+    if (proElem.length >= 30) {
+        productName[i].innerHTML = proElem.slice(0, 30) + " ...";
     }
 }
