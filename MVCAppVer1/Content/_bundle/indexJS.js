c8db66d50cdd6f61207bbc42a98d6e9b440497ec
thuantran664488 <trandinhthuan0112@gmail.com>
2021-07-19 11:40:10 +0700

    change ...

--- MVCAppVer1/Content/_bundle/indexJS.js
+++ MVCAppVer1/Content/_bundle/indexJS.js
@@ -3,15 +3,6 @@
 
 'use strict'
 
-let productName = document.getElementsByClassName("product-name");
-
-for (let i = 0; i < productName.length; i++) {
-    proElem = productName[i].innerHTML;
-    if (proElem.length >= 30) {
-        productName[i].innerHTML = proElem.slice(0, 30) + " ...";
-    }
-}
-
 function loadDoc() {
     var xhttp = new XMLHttpRequest();
     xhttp.onload = function () {
