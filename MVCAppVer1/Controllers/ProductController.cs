59d8008e895582c84e93b3dbdff72b45f419394b
thuantran664488 <trandinhthuan0112@gmail.com>
2021-07-26 14:11:25 +0700

    adjust css

--- MVCAppVer1/Controllers/ProductController.cs
+++ MVCAppVer1/Controllers/ProductController.cs
@@ -20,8 +20,9 @@ namespace MVCAppVer1.Controllers
             if (string.IsNullOrEmpty(id)) return null;
 
             var getAll = Webservice.Helper.getProducts(20, 0);
+            if (getAll == null || getAll.Total == 0 || getAll.ListProducts == null) return null;
+
             var lst = getAll.ListProducts;
-            if (lst == null || getAll.Total == 0) return null;
 
             var obj = lst.FirstOrDefault(p => p != null && p.Id.Equals(id) && p.Url.Equals(url));
             if (obj == null) return null;
@@ -29,4 +30,4 @@ namespace MVCAppVer1.Controllers
             return View(obj);
         }
     }
-} 
+}
