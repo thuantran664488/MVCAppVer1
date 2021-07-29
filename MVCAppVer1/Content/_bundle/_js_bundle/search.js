14272468ffb1757936542224cf3e260fc720706f
thuantran664488 <trandinhthuan0112@gmail.com>
2021-07-29 11:10:56 +0700

    add radio button

--- MVCAppVer1/Content/_bundle/_js_bundle/search.js
+++ MVCAppVer1/Content/_bundle/_js_bundle/search.js
@@ -51,36 +51,65 @@ $('.btn-change-price').keydown(function (e) {
     }
 })
 
+function callAjaxForPrice(PageIndex, keyword, minPrice, maxPrice) {
+    $.ajax({
+        type: 'POST', //POST
+        url: '/search/filter/',
+        data: { PageIndex: PageIndex, keyword: keyword, minPrice: minPrice, maxPrice: maxPrice },
+        cache: false,
+        success: function (result) {
+            if (result.html != null && result.remain >= 0) {
+                $('.grid-container').empty();
+                $('.grid-container').append(result.html);
+                $('#remain').html(result.remain);
+                if (result.remain == 0) {
+                    $('#btnViewMore').hide();
+                } else {
+                    $('#btnViewMore').show();
+                }
+            } else {
+                $('.grid-container').empty();
+                $('#btnViewMore').hide();
+            }
+        }
+    });
+}
+
+$('input[type=radio]').on('change', function () {
+    PageIndex = 1;
+    let keyword = getSearchKeyword();
+    radioValue = $('input[name=radioName]:checked').val();
+    if (radioValue == "all") {
+        minPrice = 0;
+        maxPrice = 1 * 10 ^ 10;
+    }
+    if (radioValue == "under100") {
+        minPrice = 0;
+        maxPrice = 100;
+    }
+    if (radioValue == "between100and500") {
+        minPrice = 100;
+        maxPrice = 500;
+    }
+    if (radioValue == "between500and1mil") {
+        minPrice = 500;
+        maxPrice = 1000;
+    }
+    if (radioValue == "above1mil") {
+        minPrice = 1000;
+        maxPrice = 1 * 10 ^ 10;
+    }
+    callAjaxForPrice(0, keyword, minPrice, maxPrice);
+});
+
 $("#btnSearchByPrice").click(function () {
     PageIndex = 1;
     let keyword = getSearchKeyword();
     minPrice = Number($('#input-minPrice').val());
-    console.log(minPrice);
     maxPrice = Number($('#input-maxPrice').val());
-    console.log(maxPrice);
-    console.log(minPrice < maxPrice);
     if (minPrice < maxPrice) {
-        $.ajax({
-            type: 'POST', //POST
-            url: '/search/filter/',
-            data: { PageIndex: 0, keyword: keyword, minPrice: minPrice, maxPrice: maxPrice },
-            cache: false,
-            success: function (result) {
-                if (result.html != null && result.remain >= 0) {
-                    $('.grid-container').empty();
-                    $('.grid-container').append(result.html);
-                    $('#remain').html(result.remain);
-                    if (result.remain == 0) {
-                        $('#btnViewMore').hide();
-                    } else {
-                        $('#btnViewMore').show();
-                    }
-                } else {
-                    $('.grid-container').empty();
-                    $('#btnViewMore').hide();
-                }
-            }
-        });
+        callAjaxForPrice(0, keyword, minPrice, maxPrice);
+
     } else alert("Your min is greater or equal max\n Please check your min or max price again")
 })
 
@@ -94,9 +123,6 @@ $("#btnViewMore").click(function () {
         cache: false,
         success: function (result) {
             if (result.html != null && result.remain >= 0) {
-                console.log(" HTML=  " + result.html);
-                console.log(" Remain=  " + result.remain);
-                console.log(" Total=  " + result.total);
                 $('.grid-container').append(result.html);
                 $('#remain').html(result.remain);
                 if (result.remain == 0) {
